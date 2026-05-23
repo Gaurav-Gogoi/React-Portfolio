6e3b1f9ba6fcfb49443fdc211ebba4b76c2f367b
Gaurav Gogoi <gauravgogoi072@gmail.com>
2026-05-23 07:06:30 +0530

    spelling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -21,7 +21,7 @@ export const HeroSection = () => {
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-          I'm a Fullstaack Developer who crafts clean, responsive, 
+          I'm a Fullstack Developer who crafts clean, responsive, 
             and engaging web interfaces using modern technologies 
             like HTML, CSS, JavaScript, and React.
           </p>
